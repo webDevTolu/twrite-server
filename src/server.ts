9d98ead29725a9e88bdf0d918912a25ac6523e68
tolu-c <tolu.adeyemo7@gmail.com>
2023-05-06 02:53:43 +0100

    setting graphql server -1

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log("hello world");
+import express from "express";
+import cors from "cors";
+import { graphqlHTTP } from 'express-graphql'
+
+const app = express();
+
+// connect to db
+
+// middleware
+app.use(cors());
+
+app.use('/api', graphqlHTTP({
+  schema: 
+}))
+
+const PORT = process.env.PORT || 6000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
